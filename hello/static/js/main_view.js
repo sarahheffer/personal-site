76d3b6cffa1d388db2ad35edb11b18b8137564e2
Sarah Heffer <sarahheffer1212@hotmail.com>
2015-09-16 14:28:14 -0400

    Change about me font, add paragraph break

--- hello/static/js/main_view.js
+++ hello/static/js/main_view.js
@@ -6,7 +6,7 @@ define([
     return Marionette.ItemView.extend({
         template: '#main-template',
         templateHelpers: {
-            aboutMeText: "In 2010 I discovered the world of computer science through " +
+            aboutMePOne: "In 2010 I discovered the world of computer science through " +
 "a single course offered at my high school, and I was hooked immediately. In 2012 I " +
 "was accepted into Honours Computer Science at the University of Waterloo, and I’m " +
 "currently completing my 3rd year of study. I am in the co-op program and I feel " +
@@ -15,9 +15,8 @@ define([
 "in San Francisco, has exposed me to unique challenges, new technologies, as well as " +
 "many aspects of the software development lifecycle. I have been fortunate to work " +
 "with fabulous, brilliant, creative people who have mentored me and challenged me " +
-"along the way to help me rapidly gain experience in both mobile and web development." +
-"\n" +
-"I try to give back wherever I can, and have been a volunteer math tutor and peer " +
+"along the way to help me rapidly gain experience in both mobile and web development.",
+            aboutMePTwo: "I try to give back wherever I can, and have been a volunteer math tutor and peer " +
 "mentor for at-risk youth, and a volunteer TA teaching Android development to " +
 "underrepresented communities in technology, as well as an animal socialization " +
 "volunteer for Rabbit Rescue. Some things that make me happy include the great " +
